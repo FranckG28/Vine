e86072e273f8a7a0af92b7df67a391e09863e607
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-01-25 08:51:05 +0100

    feat(index): add publication date

--- components/home/products-grid.tsx
+++ components/home/products-grid.tsx
@@ -1,3 +1,4 @@
+import { dateToString } from "@/lib/utils";
 import { Product } from "models/product";
 import ErrorDisplay from "../shared/error-display";
 import Card from "./card";
@@ -14,11 +15,11 @@ export default function ProductsGrid({ error, products }: { products: Product[],
 
     return (
         <div className="my-10 grid w-full max-w-screen-xl animate-[slide-down-fade_0.5s_ease-in-out] grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
-            {products.map(({ attributes: { title, link, image } }, index) => (
+            {products.map(({ attributes: { title, link, image, updatedAt } }, index) => (
                 <a href={link} key={index} target="_blank" rel="noreferrer">
                     <Card
                         title={title}
-                        description=""
+                        description={dateToString(new Date(Date.parse(updatedAt)))}
                         demo={<img src={image}></img>}
                     />
                 </a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -90,7 +90,7 @@ export default function Home(props: { products: Product[], error: string, pageCo
           </Balancer>
         </motion.p>
         <motion.input
-          className="hidden sm:flex items-center w-72 text-left space-x-3 px-4 h-12 border-slate-500 bg-white ring-1 ring-slate-900/10 hover:ring-slate-300 focus:outline-none focus:ring-2 focus:ring-sky-500 shadow-sm rounded-lg text-slate-800 placeholder:text-slate-400 dark:bg-slate-800 dark:ring-0 dark:text-slate-300 dark:highlight-white/5 dark:hover:bg-slate-700 mx-auto"
+          className="sm:flex items-center w-72 text-left space-x-3 px-4 h-12 border-slate-500 bg-white ring-1 ring-slate-900/10 hover:ring-slate-300 focus:outline-none focus:ring-2 focus:ring-sky-500 shadow-sm rounded-lg text-slate-800 placeholder:text-slate-400 dark:bg-slate-800 dark:ring-0 dark:text-slate-300 dark:highlight-white/5 dark:hover:bg-slate-700 mx-auto"
           variants={FADE_DOWN_ANIMATION_VARIANTS}
           type="text"
           placeholder="Rechercher un produit ..."
