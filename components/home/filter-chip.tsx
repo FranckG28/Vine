cfc53bc5ee8678def5022355ec178552ee123c81
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-02-20 22:27:00 +0100

    feat(filter): improve filter ux

--- components/home/filter-chip.tsx
+++ components/home/filter-chip.tsx
@@ -1,7 +1,8 @@
 import { FastFilter } from "models/fast-filter"
+import { Eye, EyeOff } from "lucide-react"
 
-const inactiveStyle = "bg-red-50 text-red-700 border-red-200";
-const activeStyle = "bg-red-500 text-white border-red-600 shadow-lg";
+const inactiveStyle = "bg-indigo-50 text-indigo-700 border-indigo-200";
+const activeStyle = "bg-indigo-500 text-white border-indigo-600 shadow-lg";
 
 export default function FilterChip({
     fastFilter, active, onClick
@@ -10,7 +11,10 @@ export default function FilterChip({
     active: boolean,
     onClick: () => void
 }) {
-    return <button onClick={onClick} className={`rounded-full text-lg transition duration-75 px-3 py-1 border ${active ? activeStyle : inactiveStyle}`}>
+    return <button onClick={onClick} className={`rounded-full flex items-center gap-2 text-lg transition duration-75 px-3 py-1 border ${active ? activeStyle : inactiveStyle}`}>
+        <div className={`${active ? "" : "opacity-50"}`}>
+            {active ? <EyeOff className="h-6 w-6" /> : <Eye className="h-6 w-6" />}
+        </div>
         {fastFilter.name}
-    </button>
+    </button >
 }
