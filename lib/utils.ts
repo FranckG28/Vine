3c0776aa8b5db1e4378ed32bd374f5584ecef4e6
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-02-27 11:12:36 +0100

    remove useless function

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,28 +18,6 @@ export const getLatestDate = (dates: Date[]): Date => {
   return dates.reduce((a, b) => (a > b ? a : b), new Date(0));
 };
 
-export async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit,
-): Promise<JSON> {
-  const res = await fetch(input, init);
-
-  if (!res.ok) {
-    const json = await res.json();
-    if (json.error) {
-      const error = new Error(json.error) as Error & {
-        status: number;
-      };
-      error.status = res.status;
-      throw error;
-    } else {
-      throw new Error("An unexpected error occurred");
-    }
-  }
-
-  return res.json();
-}
-
 export function nFormatter(num: number, digits?: number) {
   if (!num) return "0";
   const lookup = [
