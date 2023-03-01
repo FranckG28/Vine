e308e010ccb8bdafb68227297bf2fa7174a4bed2
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-03-01 08:58:16 +0100

    fix updated dot condition

--- components/home/products-grid.tsx
+++ components/home/products-grid.tsx
@@ -18,14 +18,17 @@ export default function ProductsGrid({ error, products, hightlighter }: {
         return <ErrorDisplay error="No products found" />
     }
 
+
     return (
         <div className="my-10 grid w-full max-w-screen-xl animate-[slide-down-fade_0.5s_ease-in-out] grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
-            {products.map((product, index) => (
-                <span className="relative inline-flex mt-1 mb-0 hover:mt-0 hover:mb-1 transition-all" key={index}>
-                    <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1 z-10 rounded-full bg-indigo-500"></span>
+            {products.map((product, index) => {
+
+                const isHighlighted = hightlighter(product);
+                return <span className="relative inline-flex mt-1 mb-0 hover:mt-0 hover:mb-1 transition-all" key={index}>
+                    {isHighlighted && <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1 z-10 rounded-full bg-indigo-500"></span>}
                     <Link href={product.attributes.link} target="_blank" rel="noreferrer" className="w-full h-full">
                         <Card
-                            className={hightlighter(product) ? "border border-indigo-400/30 shadow-xl shadow-indigo-400/30" : ""}
+                            className={isHighlighted ? "border border-indigo-400/30" : ""}
                             title={product.attributes.title}
                             description={dateToString(new Date(Date.parse(product.attributes.updatedAt))) + ((!product.attributes.page) ? " " : " - *page " + product.attributes.page + "*")}
                             // eslint-disable-next-line @next/next/no-img-element
@@ -33,7 +36,9 @@ export default function ProductsGrid({ error, products, hightlighter }: {
                         />
                     </Link>
                 </span>
-            ))}
+
+            }
+            )}
         </div>
 
     );
